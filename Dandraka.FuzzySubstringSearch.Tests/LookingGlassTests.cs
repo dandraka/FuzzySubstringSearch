35649803128c2f08686b34a4f96b7c286e1b074e
Jim (Dimitrios) Andrakakis <dandraka@gmail.com>
2024-04-15 13:22:17 +0000

    first working version

--- Dandraka.FuzzySubstringSearch.Tests/LookingGlassTests.cs
+++ Dandraka.FuzzySubstringSearch.Tests/LookingGlassTests.cs
@@ -1,10 +1,43 @@
 namespace Dandraka.FuzzySubstringSearch.Tests;
 
+using Dandraka.FuzzySubstringSearch;
+using FluentAssertions;
+
 public class LookingGlassTests
 {
     [Fact]
-    public void Test1()
+    public void T01_SingleWordVerbatimSearch()
     {
+        var g = new LookingGlass();
 
+        int m = g.NGram("The quick brown fox jumps over the lazy dog", "quick", 2);
+        m.Should().Be(100, "the word exists in text");
     }
+
+    [Fact]
+    public void T02_DoubleWordVerbatimSearch()
+    {
+        var g = new LookingGlass();
+
+        int m = g.NGram("The quick brown fox jumps over the lazy dog", "lazy dog", 2);
+        m.Should().Be(100, "the words exist in text");
+    }  
+
+    [Fact]
+    public void T03_SingleWordPartialSearch()
+    {
+        var g = new LookingGlass();
+
+        int m = g.NGram("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "dolar", 2);
+        m.Should().Be(50, "the 3-gram overlap is 50%");
+    }        
+
+    [Fact]
+    public void T04_DoubleWordPartialSearch()
+    {
+        var g = new LookingGlass();
+
+        int m = g.NGram("the projekt XYZ was finished last year", "project XYZ");
+        m.Should().Be(67, "the 3-gram overlap is 66.67%");
+    }    
 }
--- Dandraka.FuzzySubstringSearch/LookingGlass.cs
+++ Dandraka.FuzzySubstringSearch/LookingGlass.cs
@@ -1,6 +1,71 @@
+using System.Diagnostics;
+
 namespace Dandraka.FuzzySubstringSearch;
 
 public class LookingGlass
 {
+    private bool isSequential;
+    public LookingGlass()
+    {
+        this.isSequential = true;
+    }
 
+    public LookingGlass(bool isSequential)
+    {
+        this.isSequential = isSequential;
+    }
+
+    public int NGram(string TargetString, string SearchString, int NGramSize = 3)
+    {
+        string t = TargetString.ToLowerInvariant();
+        string s = SearchString.ToLowerInvariant();
+        
+        int tNgramsCount = t.Length - (NGramSize-1);
+        int sNgramsCount = s.Length - (NGramSize-1);
+        
+        var tGrams = new List<string>();
+        var sGrams = new List<string>();
+
+        // Generate n-grams
+        for(int i = 0; i<tNgramsCount; i++)
+        {
+            tGrams.Add(t.Substring(i, NGramSize));
+            //Console.WriteLine(tGrams[tGrams.Count-1]);
+        }
+        for(int i = 0; i<sNgramsCount; i++)
+        {
+            sGrams.Add(s.Substring(i, NGramSize));
+            //Console.WriteLine(sGrams[sGrams.Count-1]);
+        }        
+
+        // search into target
+        int r = 0; // result count
+        int lastSearchPos = 0; // use for sequential search
+        for(int i=0; i<tNgramsCount; i++)
+        {
+            string tGram = t.Substring(i, NGramSize);
+            
+            for(int n=lastSearchPos; n<sNgramsCount; n++)
+            {
+                string sGram = s.Substring(n, NGramSize);
+                if (tGram.Equals(sGram))
+                {
+                    if (this.isSequential)
+                    {
+                        lastSearchPos = n;
+                    }
+                    r++;
+                    //Console.WriteLine("Found " + tGram + " = " + r.ToString());
+                    break;
+                }
+            }
+        }
+
+        //Console.WriteLine("r = " + r.ToString());
+        //Console.WriteLine("tNgramsCount = " + tNgramsCount.ToString());
+
+        decimal result = Math.Round(decimal.Divide(r, sNgramsCount) * 100);
+        
+        return Convert.ToInt32(result);
+    }
 }
